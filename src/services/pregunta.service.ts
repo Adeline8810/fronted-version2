fe14c8f4eb9e6820e5349e245fc8023b58ae726e
Ruth <ruthmarilsa20@gmail.com>
2025-11-20 11:21:43 +0100

    Primer commit Angular v2 con rutas actualizadas

--- src/services/pregunta.service.ts
+++ src/services/pregunta.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PreguntaService {
-  private api = 'http://localhost:8080/api/preguntas';
+  private api = 'https://app-dc5b9476-7c25-4ae3-a9cc-09c9b50d2529.cleverapps.io/api/preguntas';
 
   constructor(private http: HttpClient) {}
 
