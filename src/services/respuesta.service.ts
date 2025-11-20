c7348e57efcd7c57e191f4cf634b9aee592a0ce0
Ruth <ruthmarilsa20@gmail.com>
2025-11-20 11:38:04 +0100

    Actualizar rutas de backend y cambios recientes

--- src/services/respuesta.service.ts
+++ src/services/respuesta.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RespuestaService {
-  private api = 'https://app-dc5b9476-7c25-4ae3-a9cc-09c9b50d2529.cleverapps.io/api/respuestas';
+  private api = 'http://localhost:8080/api/respuestas';
 
   constructor(private http: HttpClient) {}
 
